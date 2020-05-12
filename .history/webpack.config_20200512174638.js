369d6fea0e9401bb5506af461cfff25b6f1ab0aa
Muki <sabljakovicmuhamed@hotmail.com>
2020-05-12 17:58:58 +0200

    create about section and initial heading stylings

--- .history/webpack.config_20200512174638.js
+++ .history/webpack.config_20200512174638.js
@@ -5,8 +5,8 @@ const postCSSPlugins = [
     require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
-    require('postcss-hexrgba'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('postcss-hexrgba')
 ]
 
 module.exports = {
