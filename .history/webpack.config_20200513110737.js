fce384d7849304619455070d482b921757e52f57
Muki <sabljakovicmuhamed@hotmail.com>
2020-05-13 11:23:09 +0200

    Complete about-section

---
+++ .history/webpack.config_20200513110737.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('postcss-hexrgba'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, './')
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./**/*.html')
+        },
+        contentBase: path.join(__dirname, './'),
+        hot: true,
+        port: 3001,
+        host: '0.0.0.0'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
+}
